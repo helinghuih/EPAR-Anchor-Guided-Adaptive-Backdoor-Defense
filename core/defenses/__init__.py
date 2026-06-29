f6b9e94dde26eac2d3f3d2a16ee0a302312f9e9b
helinghui <96905227+helinghuih@users.noreply.github.com>
2026-06-29 17:03:34 +0800

    Update __init__.py

--- core/defenses/__init__.py
+++ core/defenses/__init__.py
@@ -3,14 +3,11 @@ from .AutoEncoderDefense import AutoEncoderDefense
 from .ShrinkPad import ShrinkPad
 from .MCR import MCR
 from .FineTuning import FineTuning
-from .NAD import NAD
 from .Pruning import Pruning
-from .CutMix import CutMix
+
 from .IBD_PSC import IBD_PSC
 from .SCALE_UP import SCALE_UP
-from .REFINE import REFINE
-from .SCE_Defense import SCE_Defense
 
 __all__ = [
-    'AutoEncoderDefense', 'ShrinkPad', 'FineTuning', 'MCR', 'NAD', 'Pruning', 'ABL', 'CutMix', 'IBD_PSC', 'SCALE_UP', 'REFINE', 'SCE_Defense'
+    'AutoEncoderDefense', 'ShrinkPad', 'FineTuning', 'MCR', 'Pruning', 'ABL', 'IBD_PSC', 'SCALE_UP',
 ]
